226937330ca8eace028504a8891ac2b003217752
Frank-Tang17 <frank.tang17@gmail.com>
2022-02-22 11:15:13 -0500

    Added code for thermal communication

--- sub.py
+++ sub.py
@@ -1,11 +1,15 @@
 # python3.6
 
 import random
+import matplotlib.pyplot as plt
+from matplotlib import colors
+import numpy as np
+
 
 from paho.mqtt import client as mqtt_client
 
 
-# broker = '10.197.54.17'
+# broker = '10.194.90.55'
 broker = '127.0.0.1'
 port = 1883
 topic = "/plant"
@@ -13,6 +17,12 @@ topic = "/plant"
 client_id = f'python-mqtt-{random.randint(0, 100)}'
 username = 'test'
 password = 'test'
+heatmap_count = 0
+image = np.zeros((24,32))
+height = 0
+width = 0
+start_frame = 0
+frame = []
 
 def connect_mqtt() -> mqtt_client:
     def on_connect(client, userdata, flags, rc):
@@ -35,6 +45,17 @@ def subscribe(client: mqtt_client):
     client.subscribe(topic)
     client.on_message = on_message
 
+def subscribe_thermal(client: mqtt_client):
+    def on_message(client, userdata, msg):
+        f = open("output.txt", "a")
+        f.write(msg.payload.decode() + "\n")
+        f.close()
+        print(msg.payload.decode())
+
+    client.subscribe(topic)
+    client.on_message = on_message
+        
+
 def subscribe_image(client: mqtt_client):
     def on_message(client, userdata, msg):
         f = open("output.jpg", "wb")
@@ -46,7 +67,7 @@ def subscribe_image(client: mqtt_client):
 
 def run():
     client = connect_mqtt()
-    subscribe(client)
+    subscribe_thermal(client)
     client.loop_forever()
 
 
